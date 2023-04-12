4784a07196bffbdb6b1cb03aa540e70043ca3e99
SANJI <hoangtuan17012015@gmail.com>
2023-04-12 16:09:15 +0700

    add redux redux persist

---
+++ src/redux/store.ts
@@ -0,0 +1,32 @@
+import { testSlice } from "./slice/testSlice";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+// import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  //   stateReconciler: autoMergeLevel2,
+};
+
+const rootReducer = combineReducers({
+  test: testSlice.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      immutableCheck: false,
+      serializableCheck: false,
+      // serializableCheck: {
+      //   ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      // },
+    }),
+});
+
+export let persistor = persistStore(store);
