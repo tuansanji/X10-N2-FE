2b31e83a30a49e3beb406cd82735107927b9063f
ZKlegend <lekinhanh91@gmail.com>
2023-04-12 10:58:26 +0700

    Config lai ts

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Test from "./components/Test";
+import Register from "./pages/Register";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Test />
+      <Register />
+    </div>
+  );
+};
+
+export default App;
