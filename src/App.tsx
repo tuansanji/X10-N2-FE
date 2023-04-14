74b5001015737636d2418daa19ebf6aa305caadb
SANJI <hoangtuan17012015@gmail.com>
2023-04-14 21:04:59 +0700

    install sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import "./app.css";
-import React from "react";
-import { useSelector } from "react-redux";
-
 import LandingPage from "./routes/LandingPage";
 import MainLayout from "./routes/MainLayout";
+import React from "react";
+import { useSelector } from "react-redux";
 
 const App: React.FC = () => {
   const token = useSelector((state: any) => state.auth.token);
