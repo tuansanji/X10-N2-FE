3891f92dad8357a91e150766970b03f3b260e748
SANJI <hoangtuan17012015@gmail.com>
2023-04-15 02:51:14 +0700

    add search and sort on project page

--- src/redux/apiRequest.ts
+++ src/redux/apiRequest.ts
@@ -1,8 +1,8 @@
+import { setUserInfo } from "./slice/authSlice";
 import { AnyAction, Dispatch } from "@reduxjs/toolkit";
 import { NotificationInstance } from "antd/es/notification/interface";
 import axios from "axios";
 import { NavigateFunction } from "react-router-dom";
-import { setUserInfo } from "./slice/authSlice";
 import {
   getAllProjectError,
   getAllProjectStart,
@@ -14,11 +14,14 @@ export const getAllProject = async (
   dispatch: Dispatch<AnyAction>
 ) => {
   dispatch(getAllProjectStart());
-  console.log(process.env.REACT_APP_BACKEND_URL);
+
   try {
-    const res = await axios.get(`https://X10-server.onrender.com/project/all`, {
-      headers: { Authorization: `Bearer ${accessToken}` },
-    });
+    const res = await axios.get(
+      `${process.env.REACT_APP_BACKEND_URL}/project/all`,
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    );
     dispatch(getAllProjectSuccess(res.data));
   } catch (error) {
     dispatch(getAllProjectError());
@@ -33,7 +36,7 @@ export const requestLogin = async (
 ) => {
   try {
     const response = await axios.post(
-      "https://x10-server.onrender.com/auth/login",
+      `${process.env.REACT_APP_BACKEND_URL}/auth/login`,
       reqBody
     );
     dispatch(setUserInfo(response.data));
@@ -53,7 +56,7 @@ export const requestRegister = async (
 ) => {
   try {
     const response = await axios.post(
-      "https://x10-server.onrender.com/auth/signup",
+      `${process.env.REACT_APP_BACKEND_URL}/auth/signup`,
       reqBody
     );
     if (response.status === 200) {
