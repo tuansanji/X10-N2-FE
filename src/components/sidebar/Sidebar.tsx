03cd4cad70b367afde657e95b6b16124005cd668
SANJI <hoangtuan17012015@gmail.com>
2023-04-22 17:30:46 +0700

    get back the code from the old commit

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,35 +2,11 @@ import { getAllProject } from "../../redux/apiRequest";
 import { useAppDispatch, useAppSelector } from "../../redux/hook";
 import { changeMenu } from "../../redux/slice/menuSlice";
 import { RootState } from "../../redux/store";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { AccountBookOutlined, AppstoreOutlined } from "@ant-design/icons";
 import { Menu } from "antd";
 import axios from "axios";
 import React, { useEffect, useMemo, useState } from "react";
 import { v4 as uuid } from "uuid";
-=======
-import Loading from "../support/Loading";
-import { Button, Menu } from "antd";
-=======
-import { Menu } from "antd";
->>>>>>> ba34688 (format code)
-import axios from "axios";
-import React, { useEffect, useMemo, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { v4 as uuid } from "uuid";
-<<<<<<< HEAD
-import {
-  AccountBookOutlined,
-  AppstoreOutlined,
-  MailOutlined,
-  MenuOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
->>>>>>> b1ab2c1 (edit form stage)
-=======
-import { AccountBookOutlined, AppstoreOutlined } from "@ant-design/icons";
->>>>>>> ba34688 (format code)
 import type { MenuProps } from "antd";
 type MenuItem = Required<MenuProps>["items"][number];
 function getItem(
@@ -67,11 +43,7 @@ export interface IProject {
   status: string;
 }
 const Sidebar = () => {
-<<<<<<< HEAD
   const dispatch = useAppDispatch();
-=======
-  const dispatch = useDispatch();
->>>>>>> b1ab2c1 (edit form stage)
 
   const [sidebarData, setSidebarData] = useState<{
     projects: any;
@@ -80,7 +52,6 @@ const Sidebar = () => {
     projects: [],
     total: 0,
   });
-<<<<<<< HEAD
   const listProject = useAppSelector(
     (state: RootState) => state.project?.listProject
   );
@@ -91,19 +62,6 @@ const Sidebar = () => {
   const toggleCollapsed = () => {
     dispatch(changeMenu());
   };
-<<<<<<< HEAD
-=======
-  const listProject = useSelector(
-    (state: RootState) => state.project?.listProject
-  );
-  const statusMenu = useSelector((state: RootState) => state.menu?.status);
-  const token = useSelector((state: RootState) => state.auth.userInfo.token);
-  const reloadSidebar = useSelector((state: RootState) => state.menu.reload);
-  // đóng mở sidebar
-  const toggleCollapsed = () => {
-    dispatch(changeMenu());
-  };
->>>>>>> b1ab2c1 (edit form stage)
 
   //api chi dữ liệu cho sidebar
   useEffect(() => {
@@ -115,41 +73,10 @@ const Sidebar = () => {
         setSidebarData(response.data);
       });
   }, [listProject, reloadSidebar]);
-<<<<<<< HEAD
-=======
-  const fetchStagesData = (projectId: string): any => {
-    try {
-      axios
-        .get(
-          `${process.env.REACT_APP_BACKEND_URL}/project/stages/${projectId}`,
-          {
-            headers: { Authorization: `Bearer ${token}` },
-          }
-        )
-        .then((res) => [
-          ...res.data?.stages.map((stage: IStage, index2: number) =>
-            getItem("Submenu", `sub${index2 + 1}`, null, [
-              getItem("Option 7", index2 + 1),
-              getItem("Option 8", index2 + 1),
-            ])
-          ),
-        ]);
-    } catch (err) {
-      console.log(err);
-      return [];
-    }
-  };
->>>>>>> 6a063c4 (update stage form)
-=======
->>>>>>> b1ab2c1 (edit form stage)
   const items: MenuProps["items"] = useMemo(() => {
     let newItems =
       sidebarData.projects && sidebarData.projects.length > 0
         ? [
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b1ab2c1 (edit form stage)
             ...sidebarData.projects.map((project: IProject, index: number) => {
               return getItem(
                 project.name,
@@ -174,29 +101,6 @@ const Sidebar = () => {
                 // ]
               );
             }),
-<<<<<<< HEAD
-=======
-            ...listProject?.projects?.map(
-              (project: IProject, index: number) => {
-                return getItem(
-                  project?.name,
-                  `sub${index + 1}`,
-                  <AppstoreOutlined />,
-                  fetchStagesData(project._id)
-                  // [
-                  //   ...project.stages.map((stage: IStage, index2: number) =>
-                  //     getItem("Submenu", `sub${index2 + 1}`, null, [
-                  //       getItem("Option 7", index2 + 1),
-                  //       getItem("Option 8", index2 + 1),
-                  //     ])
-                  //   ),
-                  // ]
-                );
-              }
-            ),
->>>>>>> 6a063c4 (update stage form)
-=======
->>>>>>> b1ab2c1 (edit form stage)
 
             { type: "divider" },
             getItem("Contact", "grp", null, [getItem("Mindx", "14")], "group"),
@@ -213,15 +117,7 @@ const Sidebar = () => {
           ];
     return newItems;
   }, [listProject, sidebarData]);
-<<<<<<< HEAD
-<<<<<<< HEAD
   // khi click vào item thanh sidebar
-=======
-  // khi click vào thanh sidebar
->>>>>>> b1ab2c1 (edit form stage)
-=======
-  // khi click vào item thanh sidebar
->>>>>>> ba34688 (format code)
   const onClick: MenuProps["onClick"] = (e) => {
     console.log("click ", e);
   };
